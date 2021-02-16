99437d56b382962d4a245067a2cb0f49bb0771eb
DmitryBelkevich <harleydav@mail.ru>
2021-02-16 07:37:13 +0300

    Update MainController.java

--- spring-rest/src/main/java/com/hard/controllers/MainController.java
+++ spring-rest/src/main/java/com/hard/controllers/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class MainController {
-    @GetMapping(value = "", produces = MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8")
+    @GetMapping(value = "", produces = (MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
     public String main() {
         return "Hello World";
     }
