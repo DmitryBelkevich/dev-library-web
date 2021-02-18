73feeb33ab440744a66b0b6bbc63d4b00e066d10
hard <harleydav@mail.ru>
2021-02-19 01:59:30 +0300

    Test

--- spring-rest/project/src/test/java/com/hard/services/EntityServiceTest.java
+++ spring-rest/project/src/test/java/com/hard/services/EntityServiceTest.java
@@ -5,10 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
