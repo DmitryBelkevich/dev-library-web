3a6f2fb6390d7879564cee46d983f57df907580e
hard <harleydav@mail.ru>
2021-02-18 22:34:22 +0300

    refactoring

--- spring-rest/project/src/main/java/com/hard/controllers/MainController.java
+++ spring-rest/project/src/main/java/com/hard/controllers/MainController.java
@@ -1,15 +1,41 @@
 package com.hard.controllers;
 
+import com.hard.models.Entity;
+import com.hard.services.EntityService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/entities")
 public class MainController {
-    @GetMapping(value = "", produces = (MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
-    public String main() {
-        return "Hello World";
+    private EntityService entityService = new EntityService();
+
+    @GetMapping(value = "", produces = (MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"))
+    public ResponseEntity<Collection<Entity>> getAll() {
+        Collection<Entity> entities = entityService.getAll();
+
+        ResponseEntity responseEntity = ResponseEntity
+                .status(HttpStatus.OK)
+                .body(entities);
+
+        return responseEntity;
+    }
+
+    @GetMapping(value = "/{id}", produces = (MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"))
+    public ResponseEntity<Entity> getById(@PathVariable("id") long id) {
+        Entity entity = entityService.getById(id);
+
+        ResponseEntity responseEntity = ResponseEntity
+                .status(HttpStatus.OK)
+                .body(entity);
+
+        return responseEntity;
     }
 }
---
+++ spring-rest/project/src/main/java/com/hard/services/EntityService.java
@@ -0,0 +1,38 @@
+package com.hard.services;
+
+import com.hard.models.Entity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class EntityService {
+    private Collection<Entity> entities = new ArrayList<>();
+
+    public EntityService() {
+        for (long i = 1; i <= 10; i++) {
+            Entity entity = new Entity();
+
+            entity.setId(i);
+            entity.setTitle("Entity " + i);
+
+            entities.add(entity);
+        }
+    }
+
+    public Collection<Entity> getAll() {
+        return entities;
+    }
+
+    public Entity getById(long id) {
+        Iterator<Entity> iterator = entities.iterator();
+        while (iterator.hasNext()) {
+            Entity entity = iterator.next();
+
+            if (entity.getId() == id)
+                return entity;
+        }
+
+        return null;
+    }
+}
