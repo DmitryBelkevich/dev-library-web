ce51857469450e2d8d434168db1e7cf02b670ae4
hard <harleydav@mail.ru>
2021-02-19 22:57:21 +0300

    init mvc

--- spring-mvc/init/src/main/java/com/hard/controllers/MainController.java
+++ spring-mvc/init/src/main/java/com/hard/controllers/MainController.java
@@ -1,15 +1,14 @@
 package com.hard.controllers;
 
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class MainController {
-    @GetMapping(value = "", produces = (MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
+    @GetMapping(value = "")
     public String main() {
-        return "Hello World";
+        return "main";
     }
 }
