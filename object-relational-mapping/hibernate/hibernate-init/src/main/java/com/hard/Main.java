df5f37a142fb42e8f9e88c1fdf4bf117285f7dac
hard <harleydav@mail.ru>
2021-02-20 14:42:20 +0300

    init

--- object-relational-mapping/hibernate/hibernate-init/src/main/java/com/hard/Main.java
+++ object-relational-mapping/hibernate/hibernate-init/src/main/java/com/hard/Main.java
@@ -6,6 +6,8 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
         Configuration configuration = new Configuration()
+//                .addResource("xml_entity.hbm.xml")
+//                .addAnnotatedClass(AnnotatedEntity.class)
                 .configure();
 
         SessionFactory sessionFactory = configuration
