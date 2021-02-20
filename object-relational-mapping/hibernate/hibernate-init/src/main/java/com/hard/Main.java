ca8ef21b66d9860f48d3afc672306110378f9301
hard <harleydav@mail.ru>
2021-02-20 07:09:47 +0300

    init

--- object-relational-mapping/hibernate/hibernate-init/src/main/java/com/hard/Main.java
+++ object-relational-mapping/hibernate/hibernate-init/src/main/java/com/hard/Main.java
@@ -1,21 +1,9 @@
 package com.hard;
 
-import com.hard.models.AnnotatedEntity;
-import com.hard.models.XmlEntity;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
-    private static AnnotatedEntity annotatedEntity = new AnnotatedEntity();
-    private static XmlEntity xmlEntity = new XmlEntity();
-
-    static {
-        annotatedEntity.setTitle("entity 1");
-        xmlEntity.setTitle("entity 1");
-    }
-
     public static void main(String[] args) {
         Configuration configuration = new Configuration()
                 .configure();
@@ -23,16 +11,6 @@ public class Main {
         SessionFactory sessionFactory = configuration
                 .buildSessionFactory();
 
-        Session session = sessionFactory.openSession();
-
-        Transaction transaction = session.beginTransaction();
-
-        session.save(annotatedEntity);
-        session.save(xmlEntity);
-
-        transaction.commit();
-
-        session.close();
         sessionFactory.close();
     }
 }
